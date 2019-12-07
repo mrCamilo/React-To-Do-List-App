44457b9a6ba9474361a7b49937921a0dae718e62
Manuel Camilo <mcamilo@protonmail.com>
2019-12-07 08:17:45 -0500

    todolist component added

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import ToDo from './ToDo'
+
+function App () {
+    return <ToDo/>
+}
+
+export default App
---
+++ src/components/ToDo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ToDo(){
+    return <form>
+    To do 1
+            <h3>Take a shower</h3>
+            <input type = "radio"></input>
+            Completed?
+            <h3>Clean my room</h3>
+            <input type = "radio"></input>
+            Completed?
+            <h3>Eat 3 square meals a day</h3>
+            <input type = "radio"></input>
+            Completed?
+            <h3>Wake up at a reasonable time in the morning</h3>
+            <input type = "radio"></input>
+            Completed?</form>
+}
+
+export default ToDo
