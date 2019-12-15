b1bdc76ed0536e7838d2af92058f58feba494804
Manuel Camilo <mcamilo@protonmail.com>
2019-12-15 06:20:08 -0500

    Updates

---
+++ precache-manifest.e808a42c6b4c9c43bc1c758c48a12b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddd03f5028db2c616ee061b7498499c",
+    "url": "/To-Do/index.html"
+  },
+  {
+    "revision": "c15614fb30c767c83679",
+    "url": "/To-Do/static/css/main.109e0ddb.chunk.css"
+  },
+  {
+    "revision": "20685d3a56b818eba799",
+    "url": "/To-Do/static/js/2.ec7cd9ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/To-Do/static/js/2.ec7cd9ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c15614fb30c767c83679",
+    "url": "/To-Do/static/js/main.cca587dc.chunk.js"
+  },
+  {
+    "revision": "aefc3ea14d1abbfc3b43",
+    "url": "/To-Do/static/js/runtime-main.20cc9d3d.js"
+  }
+]);
